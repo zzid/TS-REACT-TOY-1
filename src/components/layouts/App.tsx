967ae3771c611c51f491c6692c0e8a8d8141cd18
DongYun Hwang <931010dy@gmail.com>
2021-07-30 18:28:44 +0900

    init commit ( CRA -> eject : webpack paths 설정 변경 / 필요한 라이브러리 설치 )

---
+++ src/components/layouts/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App: React.FC = () => {
+    return (
+        <div className="app">
+            <h1>zzid</h1>
+        </div>
+    );
+};
+
+export default App;
